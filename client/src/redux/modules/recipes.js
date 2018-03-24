105f2b360dbabfa3d6710a1463ccabd1b8b07999
Tom Dohnal <tomas.dohnal@gmail.com>
2018-03-24 21:45:19 +0100

    Liking recipes.

--- client/src/redux/modules/recipes.js
+++ client/src/redux/modules/recipes.js
@@ -1,9 +1,9 @@
 // @flow
-import { type Dispatch } from 'redux';
 import createReducer from '../createReducer';
 import axios from '../../axios';
 
 export const FETCH_RECIPES = 'RECIPES/FETCH_RECIPES';
+export const LIKE_RECIPE = 'RECIPES/LIKE_RECIPE';
 
 type Recipe = {
   id: number,
@@ -22,7 +22,9 @@ type FetchRecipesAction = {
   payload: Recipes,
 };
 
-export const fetchRecipes = () => (dispatch: Dispatch) =>
+type FetchRecipesDispatch = (action: FetchRecipesAction) => void;
+
+export const fetchRecipes = () => (dispatch: FetchRecipesDispatch) =>
   axios.get('/recipes').then(({ data }) => {
     dispatch({
       type: FETCH_RECIPES,
@@ -30,7 +32,41 @@ export const fetchRecipes = () => (dispatch: Dispatch) =>
     });
   });
 
+type LikeRecipeAction = {
+  type: typeof LIKE_RECIPE,
+  likedRecipeId: number,
+  payload: Recipe,
+};
+
+type LikeRecipeDispatch = (action: LikeRecipeAction) => void;
+
+export const likeRecipe = (recipeId: number, currentLikesCount: number) => (
+  dispatch: LikeRecipeDispatch,
+) =>
+  axios
+    .patch(`/recipes/${recipeId}`, { likes: currentLikesCount + 1 })
+    .then(({ data }) => {
+      dispatch({
+        type: LIKE_RECIPE,
+        likedRecipeId: recipeId,
+        payload: data,
+      });
+    });
+
 export default createReducer([], {
   [FETCH_RECIPES]: (state: Recipes, action: FetchRecipesAction): Recipes =>
     action.payload,
+  [LIKE_RECIPE]: (state: Recipes, action: LikeRecipeAction): Recipes => {
+    if (!state.length) {
+      return state;
+    }
+
+    return state.map(recipe => {
+      if (recipe.id === action.likedRecipeId) {
+        return action.payload;
+      }
+
+      return recipe;
+    });
+  },
 });
--- client/tests/redux/modules/recipes.test.js
+++ client/tests/redux/modules/recipes.test.js
@@ -1,22 +1,27 @@
 import _ from 'lodash';
 import faker from 'faker';
 
-import reducer, { FETCH_RECIPES } from '../../../src/redux/modules/recipes';
+import reducer, {
+  FETCH_RECIPES,
+  LIKE_RECIPE,
+} from '../../../src/redux/modules/recipes';
+
+const createRecipe = index => ({
+  id: index,
+  name: faker.lorem.words(3),
+  ingredients: _.times(4 + faker.random.number(6), () => faker.lorem.word()),
+  description: faker.lorem.paragraph(),
+  prepTime: (faker.random.number(2) + 1) * 10,
+  cookTime: (faker.random.number(8) + 1) * 10,
+  likes: faker.random.number(20),
+});
+
+const createRecipes = count => _.times(count, index => createRecipe(index));
 
 describe('recipes', () => {
   describe('reducer', () => {
     it('handles FETCH_RECIPES action with empty current state', () => {
-      const recipes = _.times(10, index => ({
-        id: index,
-        name: faker.lorem.words(3),
-        ingredients: _.times(4 + faker.random.number(6), () =>
-          faker.lorem.word(),
-        ),
-        description: faker.lorem.paragraph(),
-        prepTime: (faker.random.number(2) + 1) * 10,
-        cookTime: (faker.random.number(8) + 1) * 10,
-        likes: faker.random.number(20),
-      }));
+      const recipes = createRecipes(10);
 
       const fetchRecipesAction = {
         type: FETCH_RECIPES,
@@ -27,36 +32,44 @@ describe('recipes', () => {
     });
 
     it('handles FETCH_RECIPES action with non-empty current state', () => {
-      const recipes = _.times(10, index => ({
-        id: index,
-        name: faker.lorem.words(3),
-        ingredients: _.times(4 + faker.random.number(6), () =>
-          faker.lorem.word(),
-        ),
-        description: faker.lorem.paragraph(),
-        prepTime: (faker.random.number(2) + 1) * 10,
-        cookTime: (faker.random.number(8) + 1) * 10,
-        likes: faker.random.number(20),
-      }));
+      const recipes = createRecipes(10);
 
       const fetchRecipesAction = {
         type: FETCH_RECIPES,
         payload: recipes,
       };
 
-      const currentState = _.times(10, index => ({
-        id: index,
-        name: faker.lorem.words(3),
-        ingredients: _.times(4 + faker.random.number(6), () =>
-          faker.lorem.word(),
-        ),
-        description: faker.lorem.paragraph(),
-        prepTime: (faker.random.number(2) + 1) * 10,
-        cookTime: (faker.random.number(8) + 1) * 10,
-        likes: faker.random.number(20),
-      }));
+      const currentState = createRecipes(10);
 
       expect(reducer(currentState, fetchRecipesAction)).toEqual(recipes);
+    });
+
+    it('handles LIKE_RECIPE action with no current state', () => {
+      const likeRecipeAction = {
+        type: LIKE_RECIPE,
+        likedRecipeId: 5,
+        payload: createRecipe(5),
+      };
+
+      const currentState = [];
+
+      expect(reducer(currentState, likeRecipeAction)).toEqual(currentState);
+    });
+
+    it('handles LIKE_RECIPE action with current state', () => {
+      const likedRecipeId = 5;
+
+      const likeRecipeAction = {
+        type: LIKE_RECIPE,
+        likedRecipeId,
+        payload: createRecipe(5),
+      };
+
+      const currentState = createRecipes(10);
+
+      expect(reducer(currentState, likeRecipeAction)[likedRecipeId]).toEqual(
+        likeRecipeAction.payload,
+      );
     });
 
     it('returns current state when an action of an unknown type is passed', () => {
